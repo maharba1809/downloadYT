24f2c3b372ae3a17f09afb69d431eb05582c49f5
maxwell <nordik14@gmail.com>
2022-06-25 12:52:26 -0500

    new script

---
+++ downloadMp3.py
@@ -0,0 +1,18 @@
+#example: python3 dowloadMp3.py
+
+from __future__ import unicode_literals
+
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=HljSXSm6v9M&t=3050s'])
+	
